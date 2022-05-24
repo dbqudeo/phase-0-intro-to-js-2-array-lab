40df170abc08efcdcdc23a891a88df197916e35b
byeongdae yu <brianbdyu@gmail.com>
2022-05-23 22:22:30 -0400

    commit tree b47a17779cc8037b3bc9985680fc06787bfa2388

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+}
+
+function appendCat(name){
+  const newCats = [ ...cats]
+  newCats.push(name);
+  return newCats;
+}
+
+function prependCat(name){
+  const newCats = [ ...cats]
+  newCats.unshift(name);
+  return newCats;
+}
+
+function removeLastCat(){
+  const newCats = [ ...cats]
+  newCats.pop();
+  return newCats;
+}
+
+function removeFirstCat(){
+  const newCats = [ ...cats]
+  newCats.shift();
+  return newCats;
+}
+/*
+
+    describe('removeLastCat()', function () {
+      it('removes the last cat in the cats array and returns a new array, leaving the cats array unchanged', function () {
+        expect(removeLastCat()).to.have.ordered.members(["Milo", "Otis"]);
+
+        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+
+    describe('removeFirstCat()', function () {
+      it('removes the first cat from the cats array and returns a new array, leaving the cats array unchanged', function () {
+        expect(removeFirstCat()).to.have.ordered.members(["Otis", "Garfield"]);
+
+        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+  });
+});
+*/
